f1b17c1398bcb1abb5b336335bbc2224ded805aa
Konstantin Nosov <knosov@inceptumsoft.com>
2012-03-12 19:12:17 +0400

    IBANK-1524

--- src/Inceptum.Messaging/TransportManager.cs
+++ src/Inceptum.Messaging/TransportManager.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
 using Inceptum.Core.Messaging;
 using Sonic.Jms;
 
 namespace Inceptum.Messaging
 {
+   
+
     internal class TransportManager : IDisposable
     {
-        private readonly Dictionary<TransportInfo, Transport> m_Connections = new Dictionary<TransportInfo, Transport>();
+        private class ResolvedTransport
+        {
+            readonly List<string> m_KnownIds=new List<string>();
+            public IEnumerable<string> KnownIds { get { return m_KnownIds.ToArray(); } }
+            public Transport Transport { get; private set; }
+
+            public void AssignTransport(Transport transport)
+            {
+                
+                if (transport == null) throw new ArgumentNullException("transport");
+                Transport = transport;
+            }
+
+            public void AddId(string transportId)
+            {
+                if (string.IsNullOrEmpty(transportId)) throw new ArgumentNullException("transportId");
+                if(!m_KnownIds.Contains(transportId))
+                    m_KnownIds.Add(transportId);
+            }
+        }
+
+
+        private readonly Dictionary<TransportInfo, ResolvedTransport> m_Connections = new Dictionary<TransportInfo, ResolvedTransport>();
         private readonly ITransportResolver m_TransportResolver;
 
         public TransportManager(ITransportResolver transportResolver)
@@ -23,7 +48,7 @@ namespace Inceptum.Messaging
         {
             lock (m_Connections)
             {
-                foreach (var transport in m_Connections.Values)
+                foreach (var transport in m_Connections.Values.Select(t=>t.Transport).Distinct())
                 {
                     transport.Dispose();
                 }
@@ -48,33 +73,54 @@ namespace Inceptum.Messaging
             
             if (transportInfo == null)
                 throw new ConfigurationErrorsException(string.Format("Transport '{0}' is not resolvable", transportId));
-            Transport transport;
+            ResolvedTransport transport;
 
-            if (!m_Connections.TryGetValue(transportInfo, out transport) || transport == null || transport.IsDisposed)
+            bool newTransport = false;
+
+            if (!m_Connections.TryGetValue(transportInfo, out transport) || transport == null || transport.Transport.IsDisposed)
             {
                 lock (m_Connections)
                 {
-                    if (!m_Connections.TryGetValue(transportInfo, out transport) || transport == null || transport.IsDisposed)
+                    if (!m_Connections.TryGetValue(transportInfo, out transport) || transport == null || transport.Transport.IsDisposed)
                     {
-                        transport = new Transport(transportInfo, () => ProceesTarnsportFailure(transportId, transportInfo));
+                        transport = new ResolvedTransport();
                         if (m_Connections.ContainsKey(transportInfo))
                             m_Connections.Remove(transportInfo);
                         m_Connections.Add(transportInfo, transport);
                     }
                 }
+                newTransport = true;
             }
-            return transport;
+
+            lock (transport)
+            {
+                if (newTransport)
+                    transport.AssignTransport(new Transport(transportInfo, () => ProcessTransportFailure(transportInfo)));
+                transport.AddId(transportId);
+                return transport.Transport;
+            }
         }
 
-        internal virtual void ProceesTarnsportFailure(string transportId, TransportInfo transportInfo)
+        internal virtual void ProcessTransportFailure(TransportInfo transportInfo)
         {
+            ResolvedTransport transport;
             lock (m_Connections)
             {
-                //m_Connections[transportInfo] = null;
+                if(!m_Connections.TryGetValue(transportInfo, out transport))
+                    return;
                 m_Connections.Remove(transportInfo);
             }
+
             var handler = TransportEvents;
-            if (handler != null) handler(transportId, Core.Messaging.TransportEvents.Failure);
+            if (handler == null) return;
+
+            lock (transport)
+            {
+                foreach (var transportId in transport.KnownIds)
+                {
+                    handler(transportId, Core.Messaging.TransportEvents.Failure);
+                }
+            }
         }
     }
 }
