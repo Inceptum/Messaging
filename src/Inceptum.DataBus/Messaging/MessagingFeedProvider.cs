711f2e9df20f7df3f038d0c30d28038f8c15c8af
Konstantin Nosov <knosov@inceptumsoft.com>
2012-06-05 13:01:44 +0400

    refactoring

--- src/Inceptum.DataBus/Messaging/MessagingFeedProvider.cs
+++ src/Inceptum.DataBus/Messaging/MessagingFeedProvider.cs
@@ -3,27 +3,21 @@ using Inceptum.Core.Messaging;
 namespace Inceptum.DataBus.Messaging
 {
 
-    public abstract class MessagingFeedProviderBase<TData, TContext> : MessagingFeedProviderBase<TData, TData, TContext>
-    {
-        protected MessagingFeedProviderBase(IMessagingEngine messagingEngine)
-            : base(messagingEngine)
-        {
-        }
-    }
+
 
     /// <summary>
     /// Messaging feed provider
     /// </summary>
     public class MessagingFeedProvider<TData, TContext> : MessagingFeedProviderBase<TData, TContext>
     {
-        private readonly DataBusMessagingEndpoint m_Endpoint;
+        private readonly Endpoint m_Endpoint;
 
         /// <summary>
         /// Creates new instance of MessagingFeedProvider
         /// </summary>
         /// <param name="messagingEngine">Messaging engine</param>
         /// <param name="endpoint">Endpoint</param>
-        public MessagingFeedProvider(IMessagingEngine messagingEngine, DataBusMessagingEndpoint endpoint)
+        public MessagingFeedProvider(IMessagingEngine messagingEngine, Endpoint endpoint)
             : base(messagingEngine)
         {
             m_Endpoint = endpoint;
@@ -32,7 +26,7 @@ namespace Inceptum.DataBus.Messaging
         /// <summary>
         /// Returns feed provider endpoint
         /// </summary>
-        protected override DataBusMessagingEndpoint GetEndpoint(TContext context)
+        protected override Endpoint GetEndpoint(TContext context)
         {
             return m_Endpoint;
         }
