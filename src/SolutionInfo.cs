673cfece0e11c30b25a239f6fd70e452f22af56e
Konstantin Nosov <knosov@inceptumsoft.com>
2013-03-08 16:05:52 +0400

    assembly attributes files refactoring

---
+++ src/SolutionInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("InceptumSoft")]
+[assembly: AssemblyProduct("Inceptum.Framework")]
+[assembly: AssemblyCopyright("Copyright © InceptumSoft 2013")]
+[assembly: AssemblyTrademark("Inceptum.Framework")]
+[assembly: AssemblyCulture("")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
