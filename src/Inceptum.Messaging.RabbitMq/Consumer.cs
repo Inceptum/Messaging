c624cefd0949b71fb6f4640bd78e0b71c8fd06a0
Konstantin Nosov <knosov@inceptumsoft.com>
2013-04-08 12:36:10 +0400

    rabbitmq transportFactory (not complete)

---
+++ src/Inceptum.Messaging.RabbitMq/Consumer.cs
@@ -0,0 +1,73 @@
+﻿namespace Inceptum.Messaging.RabbitMq
+{
+    public class Consumer
+    {
+        protected IModel Model;
+        protected IConnection Connection;
+        protected string QueueName;
+
+        protected bool isConsuming;
+
+        // used to pass messages back to UI for processing
+        public delegate void onReceiveMessage(byte[] message);
+        public event onReceiveMessage onMessageReceived;
+
+        public Consumer(string hostName, string queueName)
+        {
+            QueueName = queueName;
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            connectionFactory.HostName = hostName;
+            Connection = connectionFactory.CreateConnection();
+            Model = Connection.CreateModel();
+            Model.BasicQos(0, 1, false);
+            bool durable = true;
+            Model.QueueDeclare(QueueName, durable, false, false, null);
+        }
+
+        //internal delegate to run the consuming queue on a seperate thread
+        private delegate void ConsumeDelegate();
+
+        public void StartConsuming()
+        {
+            isConsuming = true;
+            ConsumeDelegate c = new ConsumeDelegate(Consume);
+            c.BeginInvoke(null, null);
+        }
+
+        public void Consume()
+        {
+            QueueingBasicConsumer consumer = new QueueingBasicConsumer(Model);
+            bool autoAck = false;
+            String consumerTag = Model.BasicConsume(QueueName, autoAck, consumer);
+            while (isConsuming)
+            {
+                try
+                {
+                    RabbitMQ.Client.Events.BasicDeliverEventArgs e = (RabbitMQ.Client.Events.BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    IBasicProperties props = e.BasicProperties;
+                    byte[] body = e.Body;
+                    // ... process the message
+                    onMessageReceived(body);
+                    Model.BasicAck(e.DeliveryTag, false);
+                }
+                catch (OperationInterruptedException ex)
+                {
+                    // The consumer was removed, either through
+                    // channel or connection closure, or through the
+                    // action of IModel.BasicCancel().
+                    break;
+                }
+            }
+
+        }
+
+        public void Dispose()
+        {
+            isConsuming = false;
+            if (Connection != null)
+                Connection.Close();
+            if (Model != null)
+                Model.Abort();
+        }
+    }
+}
---
+++ src/Inceptum.Messaging.RabbitMq/RabbitMqTransportFactory.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Inceptum.Messaging.Transports;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+namespace Inceptum.Messaging.RabbitMq
+{
+    internal class Transport : ITransport
+    {
+        private readonly ConnectionFactory m_Factory;
+        private readonly IConnection m_Connection;
+        private readonly IModel m_Channel;
+
+        public Transport(string host, string username, string password)
+        {
+            if (host == null) throw new ArgumentNullException("host");
+
+            m_Factory = new ConnectionFactory { HostName = host,UserName = username,Password = password};
+            m_Connection = m_Factory.CreateConnection();
+            m_Channel = m_Connection.CreateModel();
+        }
+
+        public void Send(string destination, BinaryMessage message, int ttl, string processingGroup = null)
+        {
+            m_Channel.BasicPublish(destination, "", null, message.Bytes);
+        }
+
+        public RequestHandle SendRequest(string destination, BinaryMessage message, Action<BinaryMessage> callback,
+                                         string processingGroup = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable RegisterHandler(string destination, Func<BinaryMessage, BinaryMessage> handler,
+                                           string messageType, string processingGroup = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Subscribe(string destination, Action<BinaryMessage> callback, string messageType,
+                                     string processingGroup = null)
+        {
+            var consumer = new QueueingBasicConsumer(m_Channel);
+            //String consumerTag = m_Channel.BasicConsume(destination, false, "consumer",new Dictionary<string,string>(),consumer);
+            String consumerTag = m_Channel.BasicConsume(destination, false, consumer);
+            while (true)
+            {
+                try
+                {
+                    var e = (RabbitMQ.Client.Events.BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    IBasicProperties props = e.BasicProperties;
+                    byte[] body = e.Body;
+                    try
+                    {
+                        callback(new BinaryMessage{Bytes = body,Type = props.Type});
+                        m_Channel.BasicAck(e.DeliveryTag, false);
+                    }
+                    catch (Exception exception)
+                    {
+                       //TODO:log
+                        m_Channel.BasicCancel();
+                    }
+                }
+                catch (OperationInterruptedException ex)
+                {
+                    // The consumer was removed, either through
+                    // channel or connection closure, or through the
+                    // action of IModel.BasicCancel().
+                    break;
+                }
+            }
+
+        }
+
+
+
+        public void Dispose()
+        {
+            m_Channel.Dispose();
+            m_Connection.Dispose();
+        }
+    }
+
+    public class RabbitMqTransportFactory : ITransportFactory
+    {
+        public string Name
+        {
+            get { return "RabbitMq"; }
+        }
+
+        public ITransport Create(TransportInfo transportInfo, Action onFailure)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(EXCHANGE_NAME, "fanout");
+        }
+    }
+}
