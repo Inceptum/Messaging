d7ed3038c2317ba49db6eac3941f94f7e9d5b6f6
Konstantin Nosov <knosov@inceptumsoft.com>
2013-04-26 02:03:22 +0400

    subscription with handling of multiple message types in same queue

--- src/Inceptum.Messaging.RabbitMq.Tests/TransportTests.cs
+++ src/Inceptum.Messaging.RabbitMq.Tests/TransportTests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Inceptum.Messaging.Transports;
 using NUnit.Framework;
+using RabbitMQ.Client;
+using ThreadState = System.Threading.ThreadState;
 
 namespace Inceptum.Messaging.RabbitMq.Tests
 {
@@ -20,8 +24,106 @@ namespace Inceptum.Messaging.RabbitMq.Tests
         [Test]
         public void SendTest()
         {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("test", false, false, true, null);
+            channel.ExchangeDeclare("test", "direct");
+            channel.QueueBind("test", "test","");
+
             var transport = new Transport("localhost", "guest", "guest");
-            transport.Send("test",new BinaryMessage(){Bytes =new byte[]{0x0,0x1,0x2},Type = typeof(byte[]).AssemblyQualifiedName},0);
+            transport.Send("test",new BinaryMessage{Bytes =new byte[]{0x0,0x1,0x2},Type = typeof(byte[]).Name},0);
+            transport.Subscribe("test", message =>
+                {
+                    Console.WriteLine("message:"+message.Type);
+                },null);
         }
+
+
+        [Test]
+        public void MessageOfUnknownTypeShouldPauseProcessingTillCorrespondingHandlerIsRegisteredTest()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("test.exchange", "direct");
+            channel.QueueDeclare("test.queue", false, false, true, null);
+            channel.QueueBind("test.queue", "test.exchange", "");
+            using (var transport = new Transport("localhost", "guest", "guest"))
+            {
+                var type1Received = new AutoResetEvent(false);
+                var type2Received = new AutoResetEvent(false);
+
+                transport.Subscribe("test.queue", message => type1Received.Set(), "type1");
+
+                transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type1" }, 0);
+                Assert.That(type1Received.WaitOne(2222500), Is.True, "Message of subscribed type was not delivered");
+                transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type2" }, 0);
+                transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type1" }, 0);
+                Assert.That(type1Received.WaitOne(500), Is.False, "Message of not subscribed type has not paused processing");
+                transport.Subscribe("test.queue", message => type2Received.Set(), "type2");
+                Assert.That(type1Received.WaitOne(500), Is.True, "Processing was not resumed after handler for unknown message type was registered");
+                Assert.That(type2Received.WaitOne(500), Is.True, "Processing was not resumed after handler for unknown message type was registered");
+
+            }
+        }
+
+
+        [Test]
+        [Repeat(100)]
+        public void UnknownMessageTypHandlereWaitingDoesNotPreventTransportDisposeTest()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+                channel.ExchangeDeclare("test.exchange", "direct");
+                channel.QueueDeclare("test.queue", false, false, true, null);
+                channel.QueueBind("test.queue", "test.exchange", "");
+                var initialThreadCount = Process.GetCurrentProcess().Threads.Count;
+                Console.WriteLine(Process.GetCurrentProcess().Threads.Count);
+                ManualResetEvent received=new ManualResetEvent(false);
+                Thread connectionThread = null;
+                using (var transport = new Transport("localhost", "guest", "guest"))
+                {
+                    Console.WriteLine(Process.GetCurrentProcess().Threads.Count);
+                    transport.Subscribe("test.queue", message =>
+                        {
+                            connectionThread = Thread.CurrentThread;
+                            received.Set();
+                        }, "type1");
+                    transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type1" }, 0);
+                    Assert.That(received.WaitOne(500), Is.True, "Message was not delivered");
+                    transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type2" }, 0);
+
+                }
+                Assert.That(connectionThread.ThreadState, Is.EqualTo(ThreadState.Stopped));
+            }
+        }
+
+        [Test]
+        public void HandlerWaitStopsAndMessageOfUnknownTypeReturnsToQueueOnUnsubscribeTest()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost", UserName = "guest", Password = "guest" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.ExchangeDeclare("test.exchange", "direct");
+            channel.QueueDeclare("test.queue", false, false, false, null);
+            channel.QueueBind("test.queue", "test.exchange", "");
+            using (var transport = new Transport("localhost", "guest", "guest"))
+            {
+                var received = new AutoResetEvent(false);
+                var subscription = transport.Subscribe("test.queue", message => received.Set(), "type2");
+                transport.Send("test.exchange", new BinaryMessage { Bytes = new byte[] { 0x0, 0x1, 0x2 }, Type = "type1" }, 0);
+                Assert.That(received.WaitOne(500), Is.False, "Message of not subscribed type has not paused processing");
+                subscription.Dispose();
+                transport.Subscribe("test.queue", message => received.Set(), "type1");
+                Assert.That(received.WaitOne(500), Is.True, "Message was not returned to queue");
+            }
+            channel.QueueDelete("test.queue");
+
+
+        }
+
     }
 }
