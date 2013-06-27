0fb0bf76baebda0ffba36f0b304fa436988758e6
Mikhail Tripolitov <mtripolitov@inceptumsoft.com>
2013-06-27 14:20:20 +0400

    Add test which hanfgs messagingEngine under Inceptum.Messaging.RabbitMq.Tests

---
+++ src/Inceptum.Messaging.RabbitMq.Tests/MessagingEngineTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Inceptum.Messaging.Contract;
+using NUnit.Framework;
+
+namespace Inceptum.Messaging.RabbitMq.Tests
+{
+    [TestFixture]
+    public class MessagingEngineTests
+    {
+        [Test]
+#if !DEBUG
+        [NUnit.Framework.Timeout(5000)]
+#endif
+        public void UnknownMessageTest()
+        {
+            ITransportResolver transportResolver = new TransportResolver(new Dictionary<string, TransportInfo>()
+                {
+                    {"main",new TransportInfo("localhost", "guest", "guest", "None", "RabbitMq")}
+                });
+            var eq = new Endpoint("main", "_TEST_QUEUE", true, "json");
+            var ee = new Endpoint("main", "_TEST_EXCHANGE", true, "json");
+            
+            using (var me = new MessagingEngine(transportResolver, new RabbitMqTransportFactory()))
+            {
+                me.Send("string value", ee);
+            }
+
+            using (var me = new MessagingEngine(transportResolver, new RabbitMqTransportFactory()))
+            {
+                me.Subscribe<int>(eq, Console.WriteLine);
+                me.Subscribe<double>(eq, Console.WriteLine);
+                me.Subscribe<string>(eq, Console.WriteLine);
+            }
+        }
+    }
+}
