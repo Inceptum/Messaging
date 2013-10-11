54f2bc023eeb72bb4a76e194bcb59bb7277076cb
Mikhail Tripolitov <mtripolitov@inceptumsoft.com>
2013-10-11 19:07:35 +0400

    Add EndpointResolver tests

--- src/Inceptum.Messaging.Tests/Castle/MessagingConfigurationDependsOnEndpointsTests.cs
+++ src/Inceptum.Messaging.Tests/Castle/MessagingConfigurationDependsOnEndpointsTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 using Inceptum.Messaging.Castle;
@@ -9,7 +12,7 @@ using NUnit.Framework;
 namespace Inceptum.Messaging.Tests.Castle
 {
     [TestFixture]
-    public class DependsOnEndpointsTests
+    public class MessagingConfigurationDependsOnEndpointsTests
     {
         private Endpoint m_Endpoint1;
         private Endpoint m_Endpoint2;
@@ -26,7 +29,9 @@ namespace Inceptum.Messaging.Tests.Castle
             m_Endpoint3 = new Endpoint("transport-id-3", "destination-3");
             m_Endpoint4 = new Endpoint("transport-id-4", "destination-4");
             m_Endpoint5 = new Endpoint("transport-id-5", "destination-5");
-            var endpointResolver = new EndpointResolver(new Dictionary<string, Endpoint>()
+            var messagingConfiguration = new MockMessagingConfiguration(
+                new Dictionary<string, TransportInfo>(), 
+                new Dictionary<string, Endpoint>
                 {
                     {"endpoint1", m_Endpoint1},
                     {"endpoint2", m_Endpoint2},
@@ -34,6 +39,7 @@ namespace Inceptum.Messaging.Tests.Castle
                     {"endpoint4", m_Endpoint4},
                     {"endpoint5", m_Endpoint5},
                 });
+            var endpointResolver = new EndpointResolver(messagingConfiguration.GetEndpoints());
 
             m_Container = new WindsorContainer();
             m_Container.Kernel.Resolver.AddSubResolver(endpointResolver);
@@ -42,9 +48,9 @@ namespace Inceptum.Messaging.Tests.Castle
         [Test]
         public void EndpointResolveByConstructorParameterNameTest()
         {
-            m_Container.Register(Component.For<EndpontTest1>());
+            m_Container.Register(Component.For<EndpointDependTestClass1>());
 
-            var test1 = m_Container.Resolve<EndpontTest1>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass1>();
             Assert.AreEqual(m_Endpoint1.TransportId, test1.Endpoint.TransportId);
             Assert.AreEqual(m_Endpoint1.Destination, test1.Endpoint.Destination);
         }
@@ -52,9 +58,9 @@ namespace Inceptum.Messaging.Tests.Castle
         [Test]
         public void EndpointResolveByOverridenParameterNameTest()
         {
-            m_Container.Register(Component.For<EndpontTest1>().WithEndpoints(new { endpoint1 = "endpoint2" }));
+            m_Container.Register(Component.For<EndpointDependTestClass1>().WithEndpoints(new { endpoint1 = "endpoint2" }));
 
-            var test1 = m_Container.Resolve<EndpontTest1>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass1>();
             Assert.AreEqual(m_Endpoint2.TransportId, test1.Endpoint.TransportId);
             Assert.AreEqual(m_Endpoint2.Destination, test1.Endpoint.Destination);
         }
@@ -64,8 +70,8 @@ namespace Inceptum.Messaging.Tests.Castle
         {
             var endpoint = new Endpoint(transportId: "custom-transport-id", destination: "custom-destination");
 
-            m_Container.Register(Component.For<EndpontTest1>().WithEndpoints(new { endpoint1 = endpoint }));
-            var test1 = m_Container.Resolve<EndpontTest1>();
+            m_Container.Register(Component.For<EndpointDependTestClass1>().WithEndpoints(new { endpoint1 = endpoint }));
+            var test1 = m_Container.Resolve<EndpointDependTestClass1>();
             Assert.AreEqual(endpoint.TransportId, test1.Endpoint.TransportId);
             Assert.AreEqual(endpoint.Destination, test1.Endpoint.Destination);
         }
@@ -73,9 +79,9 @@ namespace Inceptum.Messaging.Tests.Castle
         [Test]
         public void EndpointResolveByTwoDifferentConstructorParameterNameTest()
         {
-            m_Container.Register(Component.For<EndpointTest2>());
+            m_Container.Register(Component.For<EndpointDependTestClass2>());
 
-            var test1 = m_Container.Resolve<EndpointTest2>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass2>();
             Assert.AreEqual(m_Endpoint1.TransportId, test1.Endpoint1.TransportId);
             Assert.AreEqual(m_Endpoint1.Destination, test1.Endpoint1.Destination);
             Assert.AreEqual(m_Endpoint2.TransportId, test1.Endpoint2.TransportId);
@@ -85,9 +91,9 @@ namespace Inceptum.Messaging.Tests.Castle
         [Test]
         public void EndpointResolveByTwoDifferentOverridenParameterNameTest()
         {
-            m_Container.Register(Component.For<EndpointTest2>().WithEndpoints(new { endpoint1 = "endpoint4", endpoint2 = "endpoint5" }));
+            m_Container.Register(Component.For<EndpointDependTestClass2>().WithEndpoints(new { endpoint1 = "endpoint4", endpoint2 = "endpoint5" }));
 
-            var test1 = m_Container.Resolve<EndpointTest2>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass2>();
             Assert.AreEqual(m_Endpoint4.TransportId, test1.Endpoint1.TransportId);
             Assert.AreEqual(m_Endpoint4.Destination, test1.Endpoint1.Destination);
             Assert.AreEqual(m_Endpoint5.TransportId, test1.Endpoint2.TransportId);
@@ -97,9 +103,9 @@ namespace Inceptum.Messaging.Tests.Castle
         [Test]
         public void EndpointResolveByTwoDifferentOneOverridenParameterNameTest()
         {
-            m_Container.Register(Component.For<EndpointTest2>().WithEndpoints(new { endpoint2 = "endpoint5" }));
+            m_Container.Register(Component.For<EndpointDependTestClass2>().WithEndpoints(new { endpoint2 = "endpoint5" }));
 
-            var test1 = m_Container.Resolve<EndpointTest2>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass2>();
             Assert.AreEqual(m_Endpoint1.TransportId, test1.Endpoint1.TransportId);
             Assert.AreEqual(m_Endpoint1.Destination, test1.Endpoint1.Destination);
             Assert.AreEqual(m_Endpoint5.TransportId, test1.Endpoint2.TransportId);
@@ -111,9 +117,9 @@ namespace Inceptum.Messaging.Tests.Castle
         {
             var endpoint = new Endpoint(transportId: "custom-transport-id", destination: "custom-destination");
 
-            m_Container.Register(Component.For<EndpointTest2>().WithEndpoints(new { endpoint1 = "endpoint4", endpoint2 = endpoint }));
+            m_Container.Register(Component.For<EndpointDependTestClass2>().WithEndpoints(new { endpoint1 = "endpoint4", endpoint2 = endpoint }));
 
-            var test1 = m_Container.Resolve<EndpointTest2>();
+            var test1 = m_Container.Resolve<EndpointDependTestClass2>();
             Assert.AreEqual(m_Endpoint4.TransportId, test1.Endpoint1.TransportId);
             Assert.AreEqual(m_Endpoint4.Destination, test1.Endpoint1.Destination);
             Assert.AreEqual(endpoint.TransportId, test1.Endpoint2.TransportId);
@@ -121,22 +127,46 @@ namespace Inceptum.Messaging.Tests.Castle
         }
     }
 
-    internal class EndpontTest1
+    internal class MockMessagingConfiguration : IMessagingConfiguration
+    {
+        private readonly Dictionary<string, TransportInfo> m_TransportInfos;
+        private readonly Dictionary<string, Endpoint> m_Endpoints;
+
+        public MockMessagingConfiguration(
+             Dictionary<string, TransportInfo> transportInfos,
+            Dictionary<string, Endpoint> endpoints)
+        {
+            m_TransportInfos = transportInfos;
+            m_Endpoints = endpoints;
+        }
+
+        public Dictionary<string, TransportInfo> GetTransports()
+        {
+            return m_TransportInfos;
+        }
+
+        public Dictionary<string, Endpoint> GetEndpoints()
+        {
+            return m_Endpoints;
+        }
+    }
+
+    internal class EndpointDependTestClass1
     {
         public Endpoint Endpoint { get; private set; }
 
-        public EndpontTest1(Endpoint endpoint1)
+        public EndpointDependTestClass1(Endpoint endpoint1)
         {
             Endpoint = endpoint1;
         }
     }
 
-    internal class EndpointTest2
+    internal class EndpointDependTestClass2
     {
         public Endpoint Endpoint1 { get; private set; }
         public Endpoint Endpoint2 { get; private set; }
 
-        public EndpointTest2(Endpoint endpoint1, Endpoint endpoint2)
+        public EndpointDependTestClass2(Endpoint endpoint1, Endpoint endpoint2)
         {
             Endpoint1 = endpoint1;
             Endpoint2 = endpoint2;
