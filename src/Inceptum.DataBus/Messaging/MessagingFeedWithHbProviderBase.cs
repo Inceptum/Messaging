f90777c0531adcb35e303730596d66bd5ae012c3
sminkov@inceptumsoft.com <sminkov@inceptumsoft.com>
2013-12-13 11:46:43 +0400

    MessagingFeedProvider with heartbeats now supports uptime date to detect the publisher restart.

--- src/Inceptum.DataBus/Messaging/MessagingFeedWithHbProviderBase.cs
+++ src/Inceptum.DataBus/Messaging/MessagingFeedWithHbProviderBase.cs
@@ -8,7 +8,7 @@ using Inceptum.Messaging.Contract;
 
 namespace Inceptum.DataBus.Messaging
 {
-    
+
     /// <summary>
     /// Base class for messaging feed providers providing feeds that require reinitialization on hearbeat loss.
     /// </summary>
@@ -18,8 +18,8 @@ namespace Inceptum.DataBus.Messaging
     /// <typeparam name="TInitRequest">The type of the init request.</typeparam>
     /// <typeparam name="TInitResponse">The type of the init response.</typeparam>
     /// <typeparam name="THeartbeatMessage">The type of heartbeat message</typeparam>
-    public abstract class MessagingFeedWithHbProviderBase<TData, TMessage, TContext, TInitRequest, TInitResponse, THeartbeatMessage> 
-        : MessagingFeedWithInitializationBase<TData, TMessage, TContext,TInitRequest, TInitResponse>
+    public abstract class MessagingFeedWithHbProviderBase<TData, TMessage, TContext, TInitRequest, TInitResponse, THeartbeatMessage>
+        : MessagingFeedWithInitializationBase<TData, TMessage, TContext, TInitRequest, TInitResponse>
     {
 
         protected MessagingFeedWithHbProviderBase(IMessagingEngine messagingEngine, long initTimeout = 30000)
@@ -27,6 +27,10 @@ namespace Inceptum.DataBus.Messaging
         {
         }
 
+        /// <summary>
+        /// Last known uptime date
+        /// </summary>
+        protected virtual DateTime? LastKnownUptimeDate { get; set; }
 
         /// <summary>
         /// Gets the tolerance, number of intervals without HB before feed is lost.
@@ -40,26 +44,26 @@ namespace Inceptum.DataBus.Messaging
         {
 
             var hbEndpoint = GetHbEndpoint(context);
-            var heartBeatInterval=GetHeartbeatIntervalFromResponse( response, context);
-            Logger.DebugFormat("Subscribing for heartbeats. Context: {0};   Endpoint: {1}; Hb interval {2}ms", GetContextLogRepresentationString(context), hbEndpoint, heartBeatInterval);
+            var heartBeatInterval = GetHeartbeatIntervalFromResponse(response, context);
+            Logger.DebugFormat("Subscribing for heartbeats. Context: {0}; Endpoint: {1}; Hb interval {2}ms", GetContextLogRepresentationString(context), hbEndpoint, heartBeatInterval);
             var scheduledHbLoss = new SerialDisposable();
 
             var hbSubscription = Disposable.Empty;
 
-            
-                
-            var tmp= MessagingEngine.Subscribe<THeartbeatMessage>(hbEndpoint, message => processHeartBeat(context,message,dataFeed,heartBeatInterval,scheduledHbLoss));
+
+
+            var tmp = MessagingEngine.Subscribe<THeartbeatMessage>(hbEndpoint, message => processHeartBeat(context, message, dataFeed, heartBeatInterval, scheduledHbLoss));
             hbSubscription = Disposable.Create(() =>
-                                                       {
-                                                           tmp.Dispose();
-                                                           Logger.DebugFormat("Unsubscribed from heartbeats: {0};  Endpoint: {1}",GetContextLogRepresentationString(context), hbEndpoint);
-                                                       });
-          
-            scheduledHbLoss.Disposable = scheduleHbLoss( heartBeatInterval, dataFeed, context);
-            return new CompositeDisposable(hbSubscription,scheduledHbLoss);
+            {
+                tmp.Dispose();
+                Logger.DebugFormat("Unsubscribed from heartbeats: {0};  Endpoint: {1}", GetContextLogRepresentationString(context), hbEndpoint);
+            });
+
+            scheduledHbLoss.Disposable = scheduleHbLoss(heartBeatInterval, dataFeed, context);
+            return new CompositeDisposable(hbSubscription, scheduledHbLoss);
         }
 
-        private IDisposable scheduleHbLoss(long heartBeatInterval, Subject<TData> dataFeed, TContext context, DateTime? lastHb=null)
+        private IDisposable scheduleHbLoss(long heartBeatInterval, Subject<TData> dataFeed, TContext context, DateTime? lastHb = null)
         {
             var hb = lastHb;
 
@@ -67,22 +71,42 @@ namespace Inceptum.DataBus.Messaging
             return Scheduler.ThreadPool.Schedule(
                 DateTime.Now.AddMilliseconds(heartBeatInterval * Tolerance),
                 (() =>
-                     {
-                         if (hb.HasValue)
-                             Logger.WarnFormat("Heartbeats lost (no hb in x{0} intervals). Context: {1}; Interval {2}ms; The last HB was at {3}ms",Tolerance, GetContextLogRepresentationString(context), heartBeatInterval,hb.Value);
-                         else
-                             Logger.WarnFormat("There are no heartbeats for context: {0}. Cancelling subscription...", GetContextLogRepresentationString(context));
-                         dataFeed.OnError(new HeartbeatLostException(errorMessage));
-                     }
+                {
+                    if (hb.HasValue)
+                        Logger.WarnFormat("Heartbeats lost (no hb in x{0} intervals). Context: {1}; Interval {2}ms; The last HB was at {3}ms", Tolerance, GetContextLogRepresentationString(context), heartBeatInterval, hb.Value);
+                    else
+                        Logger.WarnFormat("There are no heartbeats for context: {0}. Cancelling subscription...", GetContextLogRepresentationString(context));
+                    dataFeed.OnError(new HeartbeatLostException(errorMessage));
+                }
                 ));
         }
 
         private void processHeartBeat(TContext context, THeartbeatMessage message, Subject<TData> dataFeed, long initialHeartBeatInterval, SerialDisposable scheduledHbLoss)
         {
             //Logger.DebugFormat(DateTime.Now.ToString()+": Heartbeat received. Context: {0}", GetContextLogRepresentationString(context));
-            var interval = GetHeartbeatIntervalFromHeartbeatMessage(message,context);
-            if (interval == -1)
-                interval = initialHeartBeatInterval;
+            var interval = GetHeartbeatIntervalFromHeartbeatMessage(message, context);
+            if (interval == -1) interval = initialHeartBeatInterval;
+
+            var uptimeDate = GetUptimeDateFromHeartbeatMessage(message, context);
+            if (uptimeDate != null)
+            {
+                if (LastKnownUptimeDate == null)
+                {
+                    LastKnownUptimeDate = uptimeDate;
+                }
+                else
+                {
+                    if (LastKnownUptimeDate != uptimeDate)
+                    {
+                        string errorMessage = String.Format("Uptime date changed: was {0}, received {1}. Context {2}", LastKnownUptimeDate, uptimeDate, GetContextLogRepresentationString(context));
+                        LastKnownUptimeDate = uptimeDate;
+                        dataFeed.OnError(new HeartbeatLostException(errorMessage));
+                        return;
+                    }
+                }
+            }
+
+
             scheduledHbLoss.Disposable = scheduleHbLoss(interval, dataFeed, context, DateTime.Now);
         }
 
@@ -104,5 +128,9 @@ namespace Inceptum.DataBus.Messaging
             return -1;
         }
 
+        protected virtual DateTime? GetUptimeDateFromHeartbeatMessage(THeartbeatMessage message, TContext context)
+        {
+            return null;
+        }
     }
 }
