97f4f8fd52e523e89b276f8d4d92f12198d37056
Konstantin Nosov <knosov@inceptumsoft.com>
2014-03-16 14:41:07 +0400

    ConcurrencyLevel and ThreadCount PG properties ara converted to uint

--- src/Inceptum.Messaging/Configuration/ProcessingGroupConfigurationElement.cs
+++ src/Inceptum.Messaging/Configuration/ProcessingGroupConfigurationElement.cs
@@ -5,16 +5,16 @@ namespace Inceptum.Messaging.Configuration
     public class ProcessingGroupConfigurationElement : NamedConfigurationElement
     {
         [ConfigurationProperty("concurrencyLevel", IsRequired = true, IsKey = false)]
-        public int ConcurrencyLevel
+        public uint ConcurrencyLevel
         {
-            get { return (int)this["concurrencyLevel"]; }
+            get { return (uint)this["concurrencyLevel"]; }
             set { this["concurrencyLevel"] = value; }
         }
 
         [ConfigurationProperty("queueCapacity", IsRequired = false, IsKey = false, DefaultValue = 1024)]
-        public int QueueCapacity
+        public uint QueueCapacity
         {
-            get { return (int)this["queueCapacity"]; }
+            get { return (uint)this["queueCapacity"]; }
             set { this["queueCapacity"] = value; }
         }
 
