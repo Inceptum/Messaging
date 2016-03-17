a1f2cf59a03d0b2bf833be3a1988702f65db2c4c
Konstantin Nosov <konstantin.nosov@gmail.com>
2016-03-18 02:03:09 +0300

    fixed bug where messgingSession failure callback was never called

--- src/Inceptum.Messaging.Tests/TransportManagerTests.cs
+++ src/Inceptum.Messaging.Tests/TransportManagerTests.cs
@@ -21,6 +21,7 @@ namespace Inceptum.Messaging.Tests
         {
             public const string TRANSPORT_ID1 = "tr1";
             public const string TRANSPORT_ID2 = "tr2";
+            public const string TRANSPORT_ID3 = "tr3";
             public const string USERNAME = "test";
             public const string PASSWORD = "test";
             public const string BROKER = "test";
@@ -31,9 +32,31 @@ namespace Inceptum.Messaging.Tests
             var resolver = MockRepository.GenerateMock<ITransportResolver>();
             resolver.Expect(r => r.GetTransport(TransportConstants.TRANSPORT_ID1)).Return(new TransportInfo(TransportConstants.BROKER, TransportConstants.USERNAME, TransportConstants.PASSWORD, "MachineName", "InMemory") );
             resolver.Expect(r => r.GetTransport(TransportConstants.TRANSPORT_ID2)).Return(new TransportInfo(TransportConstants.BROKER, TransportConstants.USERNAME, TransportConstants.PASSWORD, "MachineName", "InMemory") );
+            resolver.Expect(r => r.GetTransport(TransportConstants.TRANSPORT_ID3)).Return(new TransportInfo(TransportConstants.BROKER, TransportConstants.USERNAME, TransportConstants.PASSWORD, "MachineName", "Mock") );
             return resolver;
         }
-       
+
+        [Test]
+        public void MessagingSessionFailureCallbackTest()
+        {
+            var resolver = MockTransportResolver();
+            var factory=MockRepository.GenerateMock<ITransportFactory>();
+            var transport = MockRepository.GenerateMock<ITransport>();
+            Action creaedSessionOnFailure = () => { Console.WriteLine("!!"); };
+            transport.Expect(t => t.CreateSession(null)).IgnoreArguments().WhenCalled(invocation => creaedSessionOnFailure = (Action) invocation.Arguments[0]);
+            factory.Expect(f => f.Create(null, null)).IgnoreArguments().Return(transport);
+            factory.Expect(f => f.Name).Return("Mock");
+            var transportManager = new TransportManager(resolver, factory);
+            int i = 0;
+           
+            transportManager.GetMessagingSession(TransportConstants.TRANSPORT_ID3, "test", () => { Interlocked.Increment(ref i); });
+            creaedSessionOnFailure();
+            creaedSessionOnFailure();
+            
+            Assert.That(i,Is.Not.EqualTo(0),"Session failure callback was not called");
+            Assert.That(i, Is.EqualTo(1), "Session  failure callback was called twice");
+
+        }
         [Test]
         public void ConcurrentTransportResolutionTest()
         {
